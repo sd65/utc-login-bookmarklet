db8baaad5ec6d7c6aee746f64c6efceab040e635
Sylvain <sylvain.doignon@gmail.com>
2014-11-03 15:35:30 +0100

    Another commit in the wall

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
 
+	function updateDragMe() {
+		$('#dragMe').attr('href', $('#code').text());
+	}
+
 	$('input[name=username]').keyup(function() { 
 		$('#usernameField').html($.trim($(this).val()));
+		updateDragMe();
 	});
 
 	$('input[name=password]').keyup(function() { 
 		$('#passwordField').html($.trim($(this).val()));
+		updateDragMe();
 	});
 
 });
